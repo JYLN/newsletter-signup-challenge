df85996c5cec11611e88935e2174041a8bd06f39
Jaylen Baxter <14062951+JYLN@users.noreply.github.com>
2024-11-12 22:27:00 -0700

    style: :lipstick: update Tailwind config with custom font and palette
    - Install `@fontsource/roboto`
    - Import Roboto weights to layout
    - Update Tailwind config with color palette from design
    - Extend Tailwind config with Roboto font family

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import '@fontsource/roboto/400.css';
+	import '@fontsource/roboto/700.css';
 	import '../app.css';
 	let { children } = $props();
 </script>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,26 @@ export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		colors: {
+			transparent: 'transparent',
+			current: 'currentColor',
+			black: '#000000',
+			charcoalgrey: '#36384D',
+			coral: '#FF6A3A',
+			darkslategrey: '#242742',
+			grey: '#19182B',
+			strawberry: '#FF527B',
+			tomato: '#FF6155',
+			white: '#FFFFFF'
+		},
+		extend: {
+			boxShadow: {
+				button: '0 1rem 2rem 0 hsla(4, 100%, 67%, 0.5)'
+			},
+			fontFamily: {
+				roboto: ['Roboto', 'sans-serif']
+			}
+		}
 	},
 
 	plugins: [forms]
