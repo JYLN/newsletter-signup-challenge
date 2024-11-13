e4e82e17e4f26f879f6e1dfe1ffb3bf5e891cc9c
Jaylen Baxter <14062951+JYLN@users.noreply.github.com>
2024-11-12 22:13:47 -0700

    feat: :tada: init
    - Setup SvelteKit project with `sv-create` with Tailwind CSS
    - Install `sveltekit-superforms` and `valibot`
    - Import designs, assets, and starter text

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<title>Frontend Mentor | Newsletter sign-up form with success message</title>
+</svelte:head>
+
+{@render children()}
