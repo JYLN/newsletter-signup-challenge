b7fa5a1762a76d84ca11562b890815e76b0f3574
Jaylen Baxter <14062951+JYLN@users.noreply.github.com>
2024-11-13 22:52:24 -0700

    feat: :sparkles: add route protection for sign up flow
    - Add redirect to the home page if `user_email` doesn't exist to prevent rendering success page without filling out form

--- src/routes/success/+page.server.ts
+++ src/routes/success/+page.server.ts
@@ -1,7 +1,13 @@
+import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ cookies }) => {
 	const userEmail = cookies.get('user_email');
+
+	if (!userEmail) {
+		redirect(302, '/');
+	}
+
 	cookies.delete('user_email', { path: '/' });
 
 	return { userEmail };
